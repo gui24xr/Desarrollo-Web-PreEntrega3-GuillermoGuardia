211bfa0d96d8c9fb7de4ee79ac094597f4a5b434
gui24xr <guillermoxr24@gmail.com>
2023-06-11 22:43:29 -0300

    Se modifico pie, se agrego barra de controles, se agrego aside para pantallas estilo ventana, se agregaron varios controles.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const navToggle = document.querySelector(".header_nav_toggle");
+/*const navToggle = document.querySelector(".header_nav_toggle");
 const navMenu = document.querySelector(".nav_lista_secciones_sitio");
 
 //const suscripcionPanel = querySelector(".div_suscripcion");
@@ -17,7 +17,7 @@ navToggle.addEventListener("click", () => {
   } else {
     navToggle.setAttribute("aria-label", "Abrir menú");
   }
-});
+});*/
 
 
 
@@ -30,3 +30,87 @@ navToggle.addEventListener("click", () => {
 
 //})
 
+
+/* ACA LA PRUEBA -----------------------*/
+
+const navToggle = document.querySelector(".header_nav_toggle");
+const navMenu = document.querySelector(".nav_lista_secciones_sitio");
+
+const buttonActiveSuscripcion = document.querySelector(".boton_activar_suscripcion");
+const suscripcionPanel = document.querySelector(".div_suscripcion");
+
+const buttonActiveSocialBar = document.querySelector(".boton_activar_social_bar");
+const socialBar = document.querySelector(".social_bar");
+
+const buttonIntroToggle = document.querySelector(".intro_toggle")
+const buttonCloseIntro = document.querySelector(".close_intro_toggle");
+const portadaIntro = document.querySelector(".portada_intro");
+
+const buttonNoticiasToggle = document.querySelector(".noticias_toggle");
+const portadaNoticias = document.querySelector(".portada_noticias");
+const buttonCloseNoticias = document.querySelector(".close_noticias_toggle");
+
+
+//------------------------------------------------------------------------------------
+navToggle.addEventListener("click", mi_funcion);
+
+function mi_funcion(){
+
+  navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  //suscripcionPanel.classList.toggle("div_suscripcion_visible");
+}
+
+// ------------------------------------------------------------------------------------
+
+buttonActiveSuscripcion.addEventListener("click", mi_funcion2);
+
+function mi_funcion2(){
+
+  //navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  suscripcionPanel.classList.toggle("div_suscripcion_visible");
+}
+
+//-------------------------------------------------------------------------------------
+
+buttonActiveSocialBar.addEventListener("click", mi_funcion3);
+
+function mi_funcion3(){
+
+  //navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  socialBar.classList.toggle("social_bar_visible");
+}
+
+//-------------------------------------------------------------------------------------
+
+
+buttonIntroToggle.addEventListener("click", mi_funcion4);
+
+function mi_funcion4(){
+
+  //navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  portadaIntro.classList.remove("intro_visible_off");
+  portadaIntro.classList.toggle("intro_visible_on");
+}
+
+
+buttonCloseIntro.addEventListener("click", mi_funcion5);
+
+function mi_funcion5(){
+
+  //navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  //portadaIntro.classList.remove("intro_visible_on");
+  portadaIntro.classList.remove("intro_visible_on");
+  portadaIntro.classList.toggle("intro_visible_off");
+}
+
+
+//-----------------------------------------------
+
+buttonNoticiasToggle.addEventListener("click", mi_funcion6);
+
+function mi_funcion6(){
+
+  //navMenu.classList.toggle("nav_lista_secciones_sitio_visible");
+  //portadaNoticias.classList.remove("intro_visible_off");
+  portadaNoticias.classList.toggle("noticias_visible");
+}
